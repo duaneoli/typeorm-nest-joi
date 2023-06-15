91358334ade060dabcab12fb0f44113b2ec5ba7a
Eduardo Teixeira <eduardo.ateixeira@yahoo.com>
2023-06-15 09:52:14 -0300

    switching switch to object literal on JoiString function

--- src/JoiFunctions/JoiString.ts
+++ src/JoiFunctions/JoiString.ts
@@ -2,22 +2,16 @@ import * as Joi from 'joi'
 type StringType = 'any' | 'alphabet' | 'alphanumeric' | 'base64' | 'filename' | 'image' | 'uuid' | 'jwt'
 
 export const JoiString = (type: StringType = 'any') => {
-  switch (type) {
-    case 'alphabet':
-      return Joi.string().regex(/([a-zA-Z \u00C0-\u00ff]+)/)
-    case 'alphanumeric':
-      return Joi.string().regex(/([a-zA-Z0-9 \u00C0-\u00ff]+)/)
-    case 'base64':
-      return Joi.string().base64()
-    case 'filename':
-      return Joi.string().regex(/^[\w,\s-]+\.[A-Za-z]{3}$/)
-    case 'image':
-      return Joi.string().valid('image/png')
-    case 'jwt':
-      return Joi.string().regex(/(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)/)
-    case 'uuid':
-      return Joi.string().uuid()
-    default:
-      return Joi.string()
+  const stringTypes = {
+    any: Joi.string(),
+    alphabet: Joi.string().regex(/([a-zA-Z \u00C0-\u00ff]+)/),
+    alphanumeric: Joi.string().regex(/([a-zA-Z0-9 \u00C0-\u00ff]+)/),
+    base64: Joi.string().base64(),
+    filename: Joi.string().regex(/^[\w,\s-]+\.[A-Za-z]{3}$/),
+    image: Joi.string().valid('image/png'),
+    jwt: Joi.string().regex(/(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)/),
+    uuid: Joi.string().uuid(),
   }
+
+  return stringTypes[type];
 }
