e28dbd94139988a2528b577e3852d610107acce0
Duane <duane_oli@hotmail.com>
2023-09-14 09:08:04 -0300

    fix(join-filters): return undefined if array is empty

--- src/helpers/JoinFilters.ts
+++ src/helpers/JoinFilters.ts
@@ -2,8 +2,8 @@ import { FindOptionsWhere } from 'typeorm'
 import { FilterToValueOperator } from '../types'
 import { parseFilters } from './ParseFilters'
 
-export function JoinFilters<Entity>(filters: FindOptionsWhere<Entity>, filtersOr: FindOptionsWhere<Entity>): FindOptionsWhere<Entity>[] {
-  if (!filtersOr || Object.keys(filtersOr).length == 0) return [filters]
+export function JoinFilters<Entity>(filters: FindOptionsWhere<Entity>, filtersOr: FindOptionsWhere<Entity>): FindOptionsWhere<Entity>[] | undefined {
+  if (!filtersOr || Object.keys(filtersOr).length == 0) return Object.keys(filters).length > 0 ? [filters] : undefined
 
   return Object.entries(filtersOr).reduce((acc, [key, value]) => {
     acc.push({
@@ -14,7 +14,7 @@ export function JoinFilters<Entity>(filters: FindOptionsWhere<Entity>, filtersOr
   }, Array<FindOptionsWhere<Entity>>())
 }
 
-export function ParseJoinFilters<Entity>(filters: FilterToValueOperator<any>, filtersOr: FilterToValueOperator<any>): FindOptionsWhere<Entity>[] {
+export function ParseJoinFilters<Entity>(filters: FilterToValueOperator<any>, filtersOr: FilterToValueOperator<any>): FindOptionsWhere<Entity>[] | undefined {
   const where = parseFilters(filters)
   const whereOr = parseFilters(filtersOr)
   return JoinFilters(where, whereOr)
