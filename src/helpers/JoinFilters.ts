d1a84685f802fa992ec1c2ca11335961f63ae065
ferreira-guilherme-s <souza.guilhermegferreira@gmail.com>
2024-09-25 15:22:36 -0300

    Ajustando parseamento de filtersOr

--- src/helpers/JoinFilters.ts
+++ src/helpers/JoinFilters.ts
@@ -4,18 +4,34 @@ import { parseFilters } from './ParseFilters'
 
 export function JoinFilters<Entity>(filters: FindOptionsWhere<Entity>, filtersOr: FindOptionsWhere<Entity>): FindOptionsWhere<Entity>[] | undefined {
   if (!filtersOr || Object.keys(filtersOr).length == 0) return Object.keys(filters).length > 0 ? [filters] : undefined
+  const result: FindOptionsWhere<Entity>[] = [];
 
-  return Object.entries(filtersOr).reduce((acc, [key, value]) => {
-    acc.push({
-      ...filters,
-      [key]: value,
-    })
-    return acc
-  }, Array<FindOptionsWhere<Entity>>())
+  const mergeFilters = (key: string, value: any) => {
+    if (!isObject(value)) {
+      result.push({ ...filters, [key]: value });
+    } else {
+      Object.entries(value).forEach(([innerKey, innerValue]) => {
+        result.push({ ...filters, [key]: { [innerKey]: innerValue } });
+      });
+    }
+  };
+
+  Object.entries(filtersOr).forEach(([key, value]) => {
+    mergeFilters(key, value);
+  });
+
+  return result
 }
 
 export function ParseJoinFilters<Entity>(filters: FilterToValueOperator<any>, filtersOr: FilterToValueOperator<any>): FindOptionsWhere<Entity>[] | undefined {
   const where = parseFilters(filters)
   const whereOr = parseFilters(filtersOr)
   return JoinFilters(where, whereOr)
+}
+
+function isObject(value: any): boolean {
+  if (typeof value === 'object' && value !== null) {
+    return Object.keys(value).some(key => typeof value[key] === 'object' && value[key] !== null)
+  }
+  return false
 }
